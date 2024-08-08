96c3286c2188c89cc0219157feb59eacf341d832
Youcef <mtn.youssef.kh@gmail.com>
2024-08-08 17:09:49 +0200

    initial commit

---
+++ Components/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+        return (
+            <div className="footer">
+                <a href="https://twitter.com/MtnYoussef">
+                    <img src="images/Twitter.svg" alt="twitter" />
+                </a>
+                <a href="https://www.facebook.com/youssef.metane">
+                    <img src="images/Facebook.svg" alt="Facebook" />
+                </a>
+                <a href="https://www.instagram.com/metane_youcef/">
+                    <img src="images/Instagram.svg" alt="Instagram" />
+                </a>
+                <a href="https://www.instagram.com/metane_youcef/">
+                    <img src="images/Github.svg" alt="Github" />
+                </a>
+        </div>
+    )
+}
